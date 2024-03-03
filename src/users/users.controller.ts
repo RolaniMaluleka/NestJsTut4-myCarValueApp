65969bdcd29f2fa7ee87097a733b3018778e9dff
Rolani Maluleka <Rolani.Maluleka@absa.africa>
2024-03-03 23:23:53 +0200

    Also added router on controller and Post decorator on the controller.ts

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, NotAcceptableException } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 
 @Controller('auth')
@@ -7,6 +7,10 @@ export class UsersController {
     @Post('/signup')
     createUser(@Body() body: CreateUserDto) {
         console.log(body);
+
+        if(!body){
+            throw new NotAcceptableException('Email and password are of type string');
+        }
     }
 
 }
