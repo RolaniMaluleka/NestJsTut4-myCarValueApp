8f5eed8d4e7182c04dd87d73df1c650a52915443
Rolani Maluleka <Rolani.Maluleka@absa.africa>
2024-03-07 12:28:44 +0200

    added the signup and signin methods, and i will be working on those methods going forward

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,9 +1,29 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
 import { UsersService } from "./users.service";
 
 
 @Injectable()
 export class AuthService {
     constructor(private userService: UsersService){}
+
+    async signup(email: string, password: string){
+        //See if the email is in use
+        const users = await this.userService.find(email);
+        if(users.length){
+            throw new BadRequestException
+        }
+
+        //Hash the user's password
+
+        //Create a new user and save it 
+
+        //return the user
+    }
+
+    signin(){
+
+    }
+
+
 }
 
