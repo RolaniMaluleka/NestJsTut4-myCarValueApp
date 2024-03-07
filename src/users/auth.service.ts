f65182a7d62d47fda23a9d91b7a40a35d6a25bab
Rolani Maluleka <Rolani.Maluleka@absa.africa>
2024-03-07 21:47:26 +0200

    Populated the signup and signin by adding the salt and hash one user password. the tested if the salting and  hashing are really working, by signing up a new user  and signin in the very same user again, and everything worked smoothly

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,6 +1,10 @@
-import { BadRequestException, Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable, NotFoundException } from "@nestjs/common";
 import { UsersService } from "./users.service";
+import { randomBytes, scrypt as _scrypt } from "crypto"; //RandomByte will generate random number that will encrypt our password, and scrypt is our actual hashing function
+import { promisify } from "util"; //Ensure the use of promise
+import { shareReplay } from "rxjs";
 
+const scrypt = promisify(_scrypt);
 
 @Injectable()
 export class AuthService {
@@ -14,13 +18,40 @@ export class AuthService {
         }
 
         //Hash the user's password
+        //Generate a salt 
+        const salt = randomBytes(8).toString('hex');
+
+        //Hash the salt and the password together
+        const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+        // Join the hashed result and the salt together
+        const result = salt + '.' + hash.toString('hex');
 
         //Create a new user and save it 
+        const user = await this.userService.create(email,result);
+
 
         //return the user
-    }
+        return user
+    }   
 
-    signin(){
+    async signin(email: string, password: string){
+        const [user] = await this.userService.find(email)
+
+        if(!user){
+            throw new NotFoundException('User Not found')
+        }
+
+        //get back the seperated password into salt and the hash
+        const [salt, storedHash] = user.password.split('.');
+
+        const hash = (await scrypt(password, salt, 32)) as Buffer;
+        
+        if(storedHash !== hash.toString('hex')) {
+            throw new BadRequestException('Incorrect password');
+        }
+        return user;
+        
 
     }
 
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -17,17 +17,27 @@ import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { Serialize, SerializeInterceptor } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 @Serialize(UserDto) //This is to hide password in the response after a request has been made
 export class UsersController {
 
-    constructor(private userService: UsersService){}
+    constructor(
+        private userService: UsersService,
+        private authService: AuthService
+    ){}
 
     @Post('/signup')
      createUser(@Body() body: CreateUserDto) {
-       this.userService.create(body.email, body.password);
+       this.authService.signup(body.email, body.password);
     }
+
+    @Post('/signin')
+    signin(@Body() body: CreateUserDto){
+        return this.authService.signin(body.email, body.password);
+    }
+
 
     // @Serialize(UserDto)//This is to hide password in the response after a request has been made
     @Get('/:id')
